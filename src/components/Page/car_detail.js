8c900afbb12b84e610978d5fa21ac1c03e34f1be
nghiachow <nghia78x5@gmail.com>
2021-05-24 16:39:27 +0700

    create detail page

--- src/components/Page/car_detail.js
+++ src/components/Page/car_detail.js
@@ -18,65 +18,58 @@ const CarDetail = () => {
         }
     }
     return (
-        <div className="container">
-        <div className="row">
-            <div className="col-9">
-                <div className="car-detail">
-                    <div className="car">
-                            <img alt=".."  src={process.env.PUBLIC_URL + '/images/vehicle.jpg'}/>
-                            <div className="info">
-                            <p>Tên xe: Rolls Royce Phantom Wheelbase </p>
-                            <p>Cung cấp bởi Smart Rent Car Driverless Jakarta</p>
-                                    <div className="iconic">
-                                         <img alt=".." src={process.env.PUBLIC_URL + '/images/pilot.png'}/><p>Tự lái</p>
-                                         <img alt=".." src={process.env.PUBLIC_URL + '/images/wheel.png'}/><p>Số sàn</p>
-                                         <img alt=".." src={process.env.PUBLIC_URL + '/images/minicar.png'}/><p>Năm 2015 trở lên</p>
-                                    </div>
-                            </div>
-                    </div> 
-                        <div className="Policy">
-                            <h4>Thông tin thuê xe</h4>
-                            <h5>Tiện ích</h5>
-                            <p> <img alt=".." src={process.env.PUBLIC_URL + '/images/shield.png'}/>Vehicle insurance</p>
-                            <p> <img alt=".." src={process.env.PUBLIC_URL + '/images/24.png'}/>24/7 Traveloka Customer Service</p>
-                            <h5>Chính sách</h5>
-                            <p> <img alt=".." src={process.env.PUBLIC_URL + '/images/check.png'}/>Rental Requirements</p>
-                            <ul>
-                                <li>ID card (KTP or passport)</li>
-                                <li>Driver’s License (SIM A)/International Driving Permit</li>
-                                <li>Others (if provider requires additional verification)</li>
-                            </ul>
-                            <p> <img alt=".." src={process.env.PUBLIC_URL + '/images/fuel.png'}/>Return the fuel as received</p>
-                            <p> <img alt=".." src={process.env.PUBLIC_URL + '/images/clock.png'}/>Usage of up to 24 hours per rental day</p>
-                            <p> <img alt=".." src={process.env.PUBLIC_URL + '/images/cash.png'}/>Áp dụng hoàn tiền</p>
-                            <h5>Chính sách</h5>
-                            <p>Rental Requirements</p>
-                            <h5>Before Pick-up</h5>
-                            <ol>
-                                <li>The driver must share with the provider a photo of their identity card (KTP or Passport).</li>
-                                <li>The driver must share with the provider a photo of their driver’s license (SIM A) or International Driving Permit.</li>
-                                <li>The driver must pay a deposit via cash, transfer, or credit card to the provider before the rental begins.The amount will be informed by the provider after booking is finished.</li>
-                                <li>All documents presented must be original, complete, valid, and match the name used in the booking.</li>
-                            </ol>
-                            <h5>Dịch vụ thuê xe</h5>
-                            <ol>
-                                <li>Hãy chắc chắn đọc các yêu cầu về thuê xe và tài xế của nhà cung cấp, sau đó đặt xe và thanh toán tiền thuê xe. </li>
-                                <li>Sau khi thanh toán của bạn được xác nhận, hãy điền đầy đủ thông tin được yêu cầu từ nhà cung cấp xe.</li>
-                                <li>Sau khi nhà cung cấp xác minh các yêu cầu, hãy kiểm tra tình trạng xe với nhân viên của nhà cung cấp.</li>
-                                <li>Đọc và ký thỏa thuận thuê xe của nhà cung cấp, sau đó bạn có thể sử dụng dịch vụ.</li>
-                            </ol>
-                        </div> 
-                    <div className="choose-loca">
-                            <h5>Chọn địa điểm đón xe</h5>
-                            <p>Văn phòng thuê xe</p>
-                        <form>
-                            <div class="form-row">
-                                <div class="col">
-                                <input type="text" class="form-control" placeholder="Điền tên địa điểm hoặc địa chỉ"/>
+        <div className="container-fluid">
+            <div style={{marginTop:'50px'}} className="container">
+            <div className="row">
+                <div className="col-9">
+                    <div className="car-detail">
+                        <div className="car">
+                                <img alt=".."  src={process.env.PUBLIC_URL + '/images/vehicle.jpg'}/>
+                                <div className="info">
+                                <p>Tên xe: Rolls Royce Phantom Wheelbase </p>
+                                <p>Cung cấp bởi Smart Rent Car Driverless Jakarta</p>
+                                        <div className="iconic">
+                                            <img alt=".." src={process.env.PUBLIC_URL + 'https://ik.imagekit.io/tvlk/image/imageResource/2019/07/04/1562235110991-221f181276cd7208e907c33bb8554fe5.png?tr=h-24,q-75,w-24'}/><p>Tự lái</p>
+                                            <img alt=".." src={process.env.PUBLIC_URL + 'https://ik.imagekit.io/tvlk/image/imageResource/2019/10/18/1571396866495-94f335c88b623b7484537b663c79c3c8.png?tr=h-24,q-75,w-24'}/><p>Số sàn</p>
+                                            <img alt=".." src={process.env.PUBLIC_URL + 'https://ik.imagekit.io/tvlk/image/imageResource/2019/10/18/1571396866495-94f335c88b623b7484537b663c79c3c8.png?tr=h-24,q-75,w-24'}/><p>Năm 2015 trở lên</p>
+                                        </div>
                                 </div>
-                            </div>
-                        </form>
-                            <p>Địa điểm khác</p>
+                        </div> 
+                            <div className="Policy">
+                                <h4>Thông tin thuê xe</h4>
+                                <h5>Chính sách</h5>
+                                <p> <img alt=".." src={process.env.PUBLIC_URL + 'https://ik.imagekit.io/tvlk/image/imageResource/2019/10/18/1571395285230-4686f763f756af30e2cead479c2136d1.png?tr=q-75,w-24'}/>Return the fuel as received</p>
+                                <p> <img alt=".." src={process.env.PUBLIC_URL + 'https://ik.imagekit.io/tvlk/image/imageResource/2019/10/18/1571395232903-53349baf788ab844ae442df68a822cb6.png?tr=q-75,w-24'}/>Usage of up to 24 hours per rental day</p>
+                                <p> <img alt=".." src={process.env.PUBLIC_URL + 'https://ik.imagekit.io/tvlk/image/imageResource/2019/10/18/1571395182181-eb3cdc412f8fe20b9207cbcfdf80890a.png?tr=q-75,w-24'}/>Rental Requirements</p>
+                                <ul>
+                                    <li>ID card (KTP or passport)</li>
+                                    <li>Driver’s License (SIM A)/International Driving Permit</li>
+                                    <li>Others (if provider requires additional verification)</li>
+                                </ul>
+                                <p> <img alt=".." src={process.env.PUBLIC_URL + 'https://ik.imagekit.io/tvlk/image/imageResource/2020/10/28/1603886283014-038ad4e0552654b0fe681f9d4b741bd6.png?tr=q-75,w-24'}/>Áp dụng hoàn tiền</p>
+                                <h5>Tiện ích</h5>
+                                <p> <img alt=".." src={process.env.PUBLIC_URL + 'https://ik.imagekit.io/tvlk/image/imageResource/2019/10/18/1571395393133-fd2bcbd3bce1985c3bae6ea231f6969e.png?tr=q-75,w-24'}/>Vehicle insurance</p>
+                                <p> <img alt=".." src={process.env.PUBLIC_URL + 'https://ik.imagekit.io/tvlk/image/imageResource/2019/10/18/1571395341121-5ec5b1f9b4589e860127dbbdfa4527b5.png?tr=q-75,w-24'}/>24/7 Traveloka Customer Service</p>
+                                <h5>Chính sách</h5>
+                                <p>Rental Requirements</p>
+                                <h5>Before Pick-up</h5>
+                                <ol>
+                                    <li>The driver must share with the provider a photo of their identity card (KTP or Passport).</li>
+                                    <li>The driver must share with the provider a photo of their driver’s license (SIM A) or International Driving Permit.</li>
+                                    <li>The driver must pay a deposit via cash, transfer, or credit card to the provider before the rental begins.The amount will be informed by the provider after booking is finished.</li>
+                                    <li>All documents presented must be original, complete, valid, and match the name used in the booking.</li>
+                                </ol>
+                                <h5>Dịch vụ thuê xe</h5>
+                                <ol>
+                                    <li>Hãy chắc chắn đọc các yêu cầu về thuê xe và tài xế của nhà cung cấp, sau đó đặt xe và thanh toán tiền thuê xe. </li>
+                                    <li>Sau khi thanh toán của bạn được xác nhận, hãy điền đầy đủ thông tin được yêu cầu từ nhà cung cấp xe.</li>
+                                    <li>Sau khi nhà cung cấp xác minh các yêu cầu, hãy kiểm tra tình trạng xe với nhân viên của nhà cung cấp.</li>
+                                    <li>Đọc và ký thỏa thuận thuê xe của nhà cung cấp, sau đó bạn có thể sử dụng dịch vụ.</li>
+                                </ol>
+                            </div> 
+                        <div className="choose-loca">
+                                <h5>Chọn địa điểm đón xe</h5>
+                                <p>Văn phòng thuê xe</p>
                             <form>
                                 <div class="form-row">
                                     <div class="col">
@@ -84,45 +77,54 @@ const CarDetail = () => {
                                     </div>
                                 </div>
                             </form>
+                                <p>Địa điểm khác</p>
+                                <form>
+                                    <div class="form-row">
+                                        <div class="col">
+                                        <input type="text" class="form-control" placeholder="Điền tên địa điểm hoặc địa chỉ"/>
+                                        </div>
+                                    </div>
+                                </form>
+                        </div>
+                        </div>
+                        <h4 style={{marginTop:'20px'}}>Chi tiết giá</h4>
+                        <div className="pursuit">
+                            <p>Bạn thanh toán</p>
+                            <h6>560.000 VND</h6>
+                        </div>
+                        <div className="next-page">
+                            <button>Tiếp tục</button>
+                        </div>
+                    </div>     
+                <div className="col-3"> 
+                <div className="rental-info">
+                    <h5>Tóm tắt xe thuê</h5>
+                    <div  className="d-flex mb-2">
+                        <img alt=".." src={process.env.PUBLIC_URL + '/images/vehicle.jpg'}/>
+                        <div>
+                            <p style={{margin:'0'}}>Rolls Royce Phantom Wheelbase</p>
+                            <p className="badge bg-warning">Số sàn</p>
+                        </div>
                     </div>
-                    </div>
-                    <h4 style={{marginTop:'20px'}}>Chi tiết giá</h4>
-                    <div className="pursuit">
-                        <p>Bạn thanh toán</p>
-                        <h6>560.000 VND</h6>
-                    </div>
-                    <div className="next-page">
-                        <button>Tiếp tục</button>
-                    </div>
-                 </div>     
-            <div className="col-3"> 
-            <div className="rental-info">
-                <h5>Tóm tắt xe thuê</h5>
-                <div  className="d-flex mb-2">
-                    <img alt=".." src={process.env.PUBLIC_URL + '/images/vehicle.jpg'}/>
-                    <div>
-                        <p style={{margin:'0'}}>Rolls Royce Phantom Wheelbase</p>
-                        <p className="badge bg-warning">Số sàn</p>
+                    <div className="content">
+                        <ul>
+                            <li>Jakarta</li>
+                            <li>T6, 07 Th05 2021 10:30 - T7, 08 Th05 2021 10:30</li>
+                        </ul>
                     </div>
                 </div>
-                <div className="content">
-                    <ul>
-                        <li>Jakarta</li>
-                        <li>T6, 07 Th05 2021 10:30 - T7, 08 Th05 2021 10:30</li>
-                    </ul>
+                <div className="payment">
+                    <h5>Tổng giá tiền</h5>
+                    <h6>560.000 VND</h6>
+                    <button>Tiếp tục</button>
+                    <p>Đã bao gồm thuế, phí</p>
+                    <p>Giá thuê cơ bản 560.000 VND</p>
+                    <p>Bạn thanh toán 560.000VND</p>
+                </div>
                 </div>
             </div>
-            <div className="payment">
-                <h5>Tổng giá tiền</h5>
-                <h6>560.000 VND</h6>
-                <button>Tiếp tục</button>
-                <p>Đã bao gồm thuế, phí</p>
-                <p>Giá thuê cơ bản 560.000 VND</p>
-                <p>Bạn thanh toán 560.000VND</p>
-            </div>
-            </div>
-        </div>
-    </div>                
+        </div>       
+    </div>         
     );
 }
 export default CarDetail
